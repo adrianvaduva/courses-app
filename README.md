901dc881674c5cfec2809d75d981dfacc1f2dddc
Adrian Vaduva <adrian.vaduva@machteamsoft.ro>
2020-04-11 16:59:40 +0300

    Initial app structure: create React components, setup React Router and navigation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-1. Environment Build: Configure Webpack, Babel, ESLint
+Steps:
+1. Initialize project using Create React App
+2. Environment build: configure Webpack, Babel, ESLint
+3. Initial app structure: create React components, setup React Router and navigation
  
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { render } from "react-dom";
-import App from "./App";
+import {render} from "react-dom";
+import App from "./components/App";
+import {BrowserRouter as Router} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./index.css"
 
-render(<App />, document.getElementById("app"));
+render(
+    <Router>
+        <App/>
+    </Router>,
+    document.getElementById("app")
+);
